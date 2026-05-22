5fa6c19b6afd84f8284c7243ec6b6a218a258f6f
edvaldinhs <edvaldohenriqueceu@gmail.com>
2026-05-22 17:45:13 -0300

    update: hero video on scroll

--- src/App.jsx
+++ src/App.jsx
@@ -2,15 +2,54 @@ import gsap from "gsap";
 import { ScrollTrigger, SplitText } from "gsap/all";
 import Navbar from "./components/Navbar.jsx";
 import Hero from "./components/Hero.jsx";
+import { ReactLenis } from 'lenis/react';
+
+import 'lenis/dist/lenis.css';
+import {useEffect, useRef} from "react";
 
 gsap.registerPlugin(ScrollTrigger, SplitText);
 
 const App = () => {
-    return(<main>
+    const lenisRef = useRef();
+
+    useEffect(() => {
+        const lenisInstance = lenisRef.current?.lenis;
+
+        if (lenisInstance) {
+            lenisInstance.on('scroll', ScrollTrigger.update);
+
+            gsap.ticker.add((time) => {
+                lenisInstance.raf(time * 1000);
+            });
+            gsap.ticker.lagSmoothing(0);
+        }
+        return () => {
+            const lenisInstance = lenisRef.current?.lenis;
+            if (lenisInstance) {
+                // Clean up events on unmount
+                lenisInstance.off('scroll', ScrollTrigger.update);
+            }
+        };
+    }, []);
+
+    return(
+        <ReactLenis
+            root
+            ref={lenisRef}
+            options={{
+                duration: 1.2,
+                easing: (t) => Math.min(1, 1.001 - Math.pow(2, -10 * t)),
+                orientation: 'vertical',
+                smoothWheel: true,
+            }}
+        >
+        <main>
         <Navbar/>
         <Hero/>
         <div className="h-dvh bg-black"></div>
-    </main>)
+    </main>
+        </ReactLenis>
+    )
 }
 
 export default App
--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,8 +1,15 @@
 import {useGSAP} from "@gsap/react";
 import {SplitText} from "gsap/all";
 import gsap from "gsap";
+import {useRef, useState} from "react";
+import {useMediaQuery} from "react-responsive";
+import { ScrollTrigger } from "gsap/ScrollTrigger";
 
 const Hero = () => {
+    const videoRef = useRef();
+
+    const isMobile = useMediaQuery({ maxWidth: 767 });
+
     useGSAP(() =>{
         const heroSplit = new SplitText('.title', {type: 'chars, words'});
 
@@ -36,13 +43,34 @@ const Hero = () => {
         })
             .to('.right-leaf', {y: 200}, 0)
             .to('.left-leaf', {y: -200}, 0)
-    }, [])
+
+        const startValue = isMobile ? 'top 50%' : 'center 60%'
+        const endValue = isMobile ? '120% top' : 'bottom top'
+
+        const tl = gsap.timeline({
+            scrollTrigger: {
+                trigger: 'video',
+                start: startValue,
+                end: endValue,
+                scrub: true,
+                pin: true,
+            }
+        })
+
+        videoRef.current.onloadedmetadata = () => {
+            tl.to(videoRef.current, {
+                currentTime: videoRef.current.duration,
+                ease: "none",
+            })
+        }
+    }, [isMobile])
     return (
+        <>
         <section id="hero" className="noisy">
             <h1 className="title">MOJITO</h1>
 
-            <img src="images/hero-left-leaf.png" alt="left-leaf" className="left-leaf"/>
-            <img src="images/hero-right-leaf.png" alt="right-leaf" className="right-leaf"/>
+            <img src="/images/hero-left-leaf.png" alt="left-leaf" className="left-leaf"/>
+            <img src="/images/hero-right-leaf.png" alt="right-leaf" className="right-leaf"/>
             <div className="body">
                 <div className="content">
                     <div className="space-y-5 hidden md:block">
@@ -60,6 +88,16 @@ const Hero = () => {
                 </div>
             </div>
         </section>
+        <div className="video absolute inset-0">
+            <video
+                ref={videoRef}
+                src="/videos/output.mp4"
+                muted
+                playsInline
+                preload="auto"
+            />
+        </div>
+        </>
     )
 }
 export default Hero
